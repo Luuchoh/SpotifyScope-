8fa9ed91643653a902eb49c83d4b983714cd3ad4
Luis Hernandez <luxo997@gmail.com>
2025-07-17 18:22:13 -0500

    Add: front - general utils

---
+++ front/src/utils/helpers.ts
@@ -0,0 +1,44 @@
+export const formatNumber = (num: number): string => {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + 'M';
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + 'K';
+  }
+  return num.toString();
+};
+
+export const formatDuration = (ms: number): string => {
+  const minutes = Math.floor(ms / 60000);
+  const seconds = Math.floor((ms % 60000) / 1000);
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+};
+
+export const truncateText = (text: string, maxLength: number): string => {
+  if (text.length <= maxLength) return text;
+  return text.substring(0, maxLength) + '...';
+};
+
+export const getTimeRangeLabel = (timeRange: string): string => {
+  switch (timeRange) {
+    case 'short_term':
+      return 'Last 4 weeks';
+    case 'medium_term':
+      return 'Last 6 months';
+    case 'long_term':
+      return 'All time';
+    default:
+      return 'Unknown';
+  }
+};
+
+export const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+): ((...args: Parameters<T>) => void) => {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
