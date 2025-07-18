bf45bfd895591e40a79faacb4ae055c53c574a41
Luis Hernandez <luxo997@gmail.com>
2025-07-18 13:08:43 -0500

    Update: front - layout component

--- front/src/components/Layout/Layout.tsx
+++ front/src/components/Layout/Layout.tsx
@@ -7,12 +7,10 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <>
       <Header />
-      <main className="container mx-auto px-4 py-8">
         {children}
-      </main>
-    </div>
+    </>
   );
 };
 
